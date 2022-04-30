4038595b51dc1632f84c20d4740bab85f31ade76
Artur Chopikian <artur4ik303@gmail.com>
2022-04-30 15:37:57 +0300

    add comments

--- internal/delivery/grpc/handler.go
+++ internal/delivery/grpc/handler.go
@@ -3,30 +3,36 @@ package grpc_handler
 import (
 	"context"
 	"github.com/ArturChopikian/grpc-server/configs"
-	pb2 "github.com/ArturChopikian/grpc-server/internal/delivery/grpc/pb"
+	"github.com/ArturChopikian/grpc-server/internal/delivery/grpc/pb"
 	"github.com/ArturChopikian/grpc-server/internal/models"
 	"github.com/ArturChopikian/grpc-server/internal/usecase"
 	"log"
 )
 
+// ProductsHandlerInterface - represent productsHandler logic
 type ProductsHandlerInterface interface {
-	Fetch(ctx context.Context, req *pb2.FetchRequest) (*pb2.FetchResponse, error)
-	List(ctx context.Context, req *pb2.ListRequest) (*pb2.ListResponse, error)
+	Fetch(ctx context.Context, req *pb.FetchRequest) (*pb.FetchResponse, error)
+	List(ctx context.Context, req *pb.ListRequest) (*pb.ListResponse, error)
 }
 
+// productsHandler - implement handlers for ProductsService
 type productsHandler struct {
 	productsUC usecase.ProductsUCInterface
 	cfg        *configs.Config
-	pb2.UnimplementedProductsServiceServer
+	pb.UnimplementedProductsServiceServer
 }
 
+// NewProductsHandler - return pointer of productsHandler
 func NewProductsHandler(useCase *usecase.UseCases, cfg *configs.Config) *productsHandler {
 	return &productsHandler{
 		productsUC: useCase.ProductsUC,
 	}
 }
 
-func (s *productsHandler) Fetch(ctx context.Context, req *pb2.FetchRequest) (*pb2.FetchResponse, error) {
+// Fetch - get data from external URL and after create new or update exists product
+// take request with external URL and transmit to the next layer (useCases)
+// return - pb.FetchResponse with message "work" or error
+func (s *productsHandler) Fetch(ctx context.Context, req *pb.FetchRequest) (*pb.FetchResponse, error) {
 
 	err := s.productsUC.Fetch(ctx, req.GetUrl())
 	if err != nil {
@@ -34,12 +40,15 @@ func (s *productsHandler) Fetch(ctx context.Context, req *pb2.FetchRequest) (*pb
 		return nil, err
 	}
 
-	return &pb2.FetchResponse{
+	return &pb.FetchResponse{
 		Message: "Work",
 	}, nil
 }
 
-func (s *productsHandler) List(ctx context.Context, req *pb2.ListRequest) (*pb2.ListResponse, error) {
+// List - implement endless scroll
+// take pb.ListRequest with paging and ordering params
+// return list of products and number of the next page or error
+func (s *productsHandler) List(ctx context.Context, req *pb.ListRequest) (*pb.ListResponse, error) {
 
 	products, err := s.productsUC.List(ctx, req.GetOrderBy(), req.GetPageSize(), req.GetPageNumber())
 	if err != nil {
@@ -47,7 +56,7 @@ func (s *productsHandler) List(ctx context.Context, req *pb2.ListRequest) (*pb2.
 		return nil, err
 	}
 
-	return &pb2.ListResponse{
+	return &pb.ListResponse{
 		Products:       models.ProductsToGrpc(products),
 		NextPageNumber: req.GetPageNumber() + 1,
 	}, nil
